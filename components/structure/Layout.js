a2d2198afbd1d09b3e53720460c2f9867ff9d551
Adrian <adrianfcasares@gmail.com>
2021-07-03 17:32:25 -0500

    remove page transitions

--- components/structure/Layout.js
+++ components/structure/Layout.js
@@ -1,28 +1,32 @@
 import React from 'react'
 import styles from './Layout.module.css'
 
-import { useState, useEffect } from 'react'
 export default function Layout({ children }) {
 
-    const [displayChildren, setDisplayChildren] = useState(<></>)
-    const [transitionStage, setTransitionStage] = useState("fadeOut")
-
-    useEffect(() => {
-        setDisplayChildren(<h1>hello</h1>)
-    }, [])
-
-    useEffect(() => {
-        if(children !== displayChildren) {
-            
-            setTimeout(() => {
-                setDisplayChildren(children);
-            }, 1000);
-        }
-    }, [children, setDisplayChildren, displayChildren])
-
     return (
-        <div className={`${styles.Layout} ${styles[transitionStage]}`}>
-            { displayChildren }
-        </div>
+        <div className={`${styles.Layout}`}>
+            { children }
+        </div>  
     )
+    // const [displayChildren, setDisplayChildren] = useState(<></>)
+    // const [transitionStage, setTransitionStage] = useState("fadeOut")
+
+    // useEffect(() => {
+    //     setDisplayChildren(<h1>hello</h1>)
+    // }, [])
+
+    // useEffect(() => {
+    //     if(children !== displayChildren) {
+            
+    //         setTimeout(() => {
+    //             setDisplayChildren(children);
+    //         }, 1000);
+    //     }
+    // }, [children, setDisplayChildren, displayChildren])
+
+    // return (
+    //     <div className={`${styles.Layout} ${styles[transitionStage]}`}>
+    //         { displayChildren }
+    //     </div>
+    // )
 }
